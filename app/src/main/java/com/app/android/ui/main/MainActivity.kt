45f96768a0192a70716efa40d6133f63333f7f65
Tuan Nguyen Van <tuan.nnguyen@asiantech.vn>
2018-02-27 10:08:16 +0700

    Update view model

--- app/src/main/java/com/app/android/ui/main/MainActivity.kt
+++ app/src/main/java/com/app/android/ui/main/MainActivity.kt
@@ -3,12 +3,10 @@ package com.app.android.ui.main
 import android.os.Bundle
 import android.support.v7.util.DiffUtil
 import android.view.View
-import com.app.android.data.model.Task
 import com.app.android.data.source.TaskRepository
 import com.app.android.extension.observeOnUiThread
 import com.uniqlo.circle.ui.base.BaseActivity
 import org.jetbrains.anko.setContentView
-import org.jetbrains.anko.toast
 
 class MainActivity : BaseActivity() {
 
@@ -25,10 +23,6 @@ class MainActivity : BaseActivity() {
 
     override fun onBindViewModel() {
         addDisposables(
-                //Get tasks
-                viewModel.taskObservable
-                        .observeOnUiThread()
-                        .subscribe(this::handleGetTasksComplete, this::handleGetTasksError),
                 //Update progress bar status
                 viewModel.progressBarStatus
                         .observeOnUiThread()
@@ -39,22 +33,14 @@ class MainActivity : BaseActivity() {
                         .subscribe(this::handleUpdateListTask))
     }
 
-    internal fun handleSwipeRefreshLayoutOnRefresh() {
-        viewModel.getTasks()
-    }
-
-    internal fun eventTaskItemClicked() {
-    }
-
-    internal fun eventAddNewTaskClicked() {
-    }
-
     private fun handleProgressBarStatus(isShow: Boolean) {
-        ui.progressBar.run {
-            visibility = if (isShow) {
-                View.VISIBLE
-            } else {
-                View.GONE
+        when (isShow) {
+            true -> ui.progressBar.visibility = View.VISIBLE
+            else -> {
+                ui.progressBar.visibility = View.GONE
+                ui.swipeRefreshLayout.run {
+                    isRefreshing = false
+                }
             }
         }
     }
@@ -63,17 +49,13 @@ class MainActivity : BaseActivity() {
         diff.dispatchUpdatesTo(ui.taskListAdapter)
     }
 
-    private fun handleGetTasksComplete(tasks: List<Task>) {
-        toast("get tasks completed")
-        ui.swipeRefreshLayout.run {
-            isRefreshing = false
-        }
+    internal fun handleSwipeRefreshLayoutOnRefresh() {
+        viewModel.getTasks()
     }
 
-    private fun handleGetTasksError(t: Throwable) {
-        toast("get tasks failure " + t.message)
-        ui.swipeRefreshLayout.run {
-            isRefreshing = false
-        }
+    internal fun eventTaskItemClicked() {
+    }
+
+    internal fun eventAddNewTaskClicked() {
     }
 }
--- app/src/main/java/com/app/android/ui/main/MainActivityViewModel.kt
+++ app/src/main/java/com/app/android/ui/main/MainActivityViewModel.kt
@@ -3,9 +3,10 @@ package com.app.android.ui.main
 import android.support.v7.util.DiffUtil
 import com.app.android.data.model.Task
 import com.app.android.data.source.TaskRepository
+import com.app.android.extension.observeOnUiThread
 import com.uniqlo.circle.ui.base.Diff
 import io.reactivex.Observable
-import io.reactivex.schedulers.Schedulers
+import io.reactivex.ObservableEmitter
 import io.reactivex.subjects.BehaviorSubject
 import io.reactivex.subjects.PublishSubject
 
@@ -15,49 +16,38 @@ import io.reactivex.subjects.PublishSubject
  */
 class MainActivityViewModel(private val taskRepository: TaskRepository) {
     internal var progressBarStatus = BehaviorSubject.create<Boolean>()
-    internal val taskObservable = PublishSubject.create<List<Task>>()
     internal val updateListTask = PublishSubject.create<DiffUtil.DiffResult>()
     internal var tasks = mutableListOf<Task>()
 
-    init {
-        iniTasks()
-    }
-
-    private fun iniTasks() {
-        taskObservable
-                .observeOn(Schedulers.computation())
-                .flatMap {
-                    getTasksFromApi()
-                }
-                .doOnNext {
-                    val diff = Diff(tasks, it)
-                            .areItemsTheSame { oldItem, newItem ->
-                                oldItem.id == newItem.id
-                            }
-                            .areContentsTheSame { oldItem, newItem ->
-                                oldItem.title == newItem.title
-                                oldItem.description == newItem.description
-                            }
-                            .calculateDiff()
-
-                    tasks.clear()
-                    tasks.addAll(it)
-                    updateListTask.onNext(diff)
-                }
-                .subscribe()
-    }
-
     internal fun getTasks() {
-        taskObservable.onNext(tasks)
-    }
+        taskRepository.getListTask()
+                .observeOnUiThread()
+                .doOnSubscribe {
+                    progressBarStatus.onNext(true)
+                }
+                .doFinally {
+                    progressBarStatus.onNext(false)
+                }
+                .onErrorReturn {
+                    listOf()
+                }
+                .flatMap {
+                    Observable.create { e: ObservableEmitter<Task> ->
+                        val diff = Diff(tasks, it)
+                                .areItemsTheSame { oldItem, newItem ->
+                                    oldItem.id == newItem.id
+                                }
+                                .areContentsTheSame { oldItem, newItem ->
+                                    oldItem.title == newItem.title
+                                    oldItem.description == newItem.description
+                                }
+                                .calculateDiff()
 
-    private fun getTasksFromApi():
-            Observable<List<Task>> = taskRepository
-            .getListTask()
-            .doOnSubscribe {
-                progressBarStatus.onNext(true)
-            }
-            .doFinally {
-                progressBarStatus.onNext(false)
-            }
+                        tasks.clear()
+                        tasks.addAll(it)
+                        updateListTask.onNext(diff)
+                        e.onComplete()
+                    }
+                }.subscribe()
+    }
 }
