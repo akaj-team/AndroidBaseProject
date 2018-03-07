4625e938236950e9b8a86b3a784458e067b32792
at-trungnguyen <11608143+at-trungnguyen@users.noreply.github.com>
2018-03-07 17:48:54 +0700

    fix load task

--- app/src/main/java/com/app/android/ui/main/MainViewModel.kt
+++ app/src/main/java/com/app/android/ui/main/MainViewModel.kt
@@ -4,8 +4,7 @@ import android.support.v7.util.DiffUtil
 import com.app.android.data.model.Task
 import com.app.android.data.source.TaskRepository
 import com.app.android.ui.base.Diff
-import io.reactivex.Observable
-import io.reactivex.ObservableEmitter
+import io.reactivex.schedulers.Schedulers
 import io.reactivex.subjects.BehaviorSubject
 import io.reactivex.subjects.PublishSubject
 
@@ -28,25 +27,23 @@ class MainViewModel(private val taskRepository: TaskRepository) {
             .onErrorReturn {
                 listOf()
             }
-            .flatMap {
-                Observable.create { e: ObservableEmitter<Task> ->
-                    val diff = Diff(tasks, it)
-                            .areItemsTheSame { oldItem, newItem ->
-                                oldItem.id == newItem.id
-                            }
-                            .areContentsTheSame { oldItem, newItem ->
-                                oldItem.title == newItem.title
-                                oldItem.description == newItem.description
-                                oldItem.isDone == newItem.isDone
-                                oldItem.createTime == newItem.createTime
-                                oldItem.updateTime == newItem.updateTime
-                            }
-                            .calculateDiff()
+            .observeOn(Schedulers.computation())
+            .map {
+                val diff = Diff(tasks, it)
+                        .areItemsTheSame { oldItem, newItem ->
+                            oldItem.id == newItem.id
+                        }
+                        .areContentsTheSame { oldItem, newItem ->
+                            oldItem.title == newItem.title
+                            oldItem.description == newItem.description
+                            oldItem.isDone == newItem.isDone
+                            oldItem.createTime == newItem.createTime
+                            oldItem.updateTime == newItem.updateTime
+                        }
+                        .calculateDiff()
 
-                    tasks.clear()
-                    tasks.addAll(it)
-                    updateListTask.onNext(diff)
-                    e.onComplete()
-                }
+                tasks.clear()
+                tasks.addAll(it)
+                updateListTask.onNext(diff)
             }
 }
