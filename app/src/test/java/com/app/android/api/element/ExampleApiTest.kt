59d0da33433fa6bdb42fbc665cf7c4f15d4c3702
vinhhuynhl.b <vinh.huynh@asiantech.vn>
2018-03-30 08:58:00 +0700

    add jacoco

--- app/src/test/java/com/app/android/api/element/ExampleApiTest.kt
+++ app/src/test/java/com/app/android/api/element/ExampleApiTest.kt
@@ -1,6 +1,9 @@
 package com.app.android.api.element
 
 import com.uniqlo.circle.BaseTest
+import org.hamcrest.MatcherAssert
+import org.hamcrest.core.Is.`is`
+import org.junit.Test
 
 /**
  *
@@ -8,4 +11,16 @@ import com.uniqlo.circle.BaseTest
  */
 @Suppress("IllegalIdentifier")
 class ExampleApiTest : BaseTest() {
+
+    @Test
+    fun `Given a test variable  - When do nothing - Then return correct value`() {
+        /* Given */
+        val result = false
+
+        /* When */
+
+
+        /* Then */
+        MatcherAssert.assertThat(result, `is`(false))
+    }
 }
